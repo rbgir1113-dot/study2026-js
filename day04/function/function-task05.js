7c8df6fbee88e434e692a6f4b432ab2aba5e205e
rbgir1113-dot <rbgir1113@gmail.com>
2026-02-10 15:49:36 +0900

    change code

--- day04/function/function-task05.js
+++ day04/function/function-task05.js
@@ -4,25 +4,53 @@
 // ex) myFun("a", "10", "a10", 20, "1")
 // 결과: string: 1개, number 4개
 
-
 // 문자열의 메서드
 // 글자가 포함되어있는지의 여부를 true 또는 false 리턴
 // "".includes("해당글자")
-const user = (text) => {
-  let typeNum = 0;
-  let typeText = 0;
-  for(let i = 0; i < text.length; i++){
-    if(typeof(text[i]) === "number"){
-      typeNum += 1
-    }else if(typeof(text[i]) === ""+number) {
-      typeNum += 1
+const checkStringOrNumber = (...datas) => {
+  let intCount = 0
+  let floatCount = 0
+  let strCount = 0
+
+  // typeOf(NaN) === number -> true
+  for(let i = 0; i < datas.length; i++){
+    let value = datas[i]
+    if(typeof(value) === "string"){
+      
+      let includesInteger = false;
+
+      // 문자열
+      for(let i = 0; i < value.length; i++){
+        let isInt = (value[i] - 0)
+        if((value[i] - 0) === 0){
+          includesInteger = true
+          break
+        }else if(isInt){
+          includesInteger = true
+          break
+        }
+      }
+
+      if(includesInteger){
+        intCount++
+      }else{
+        strCount++
+      }
+
+
     }else{
-      typeText += 1
+      // 숫자형
+      let isFloat = String(value).includes(".")
+      if(isFloat){
+        floatCount += 1
+      }else{
+        intCount++
+      }
     }
   }
-  return typeNum && typeText;
-  
-}
-let myFun = user("a" , "10" , "a10" , 20 , "1")
 
-console.log(myFun)
+  return `정수의 개수: ${intCount}, 실수의 개수: ${floatCount}, 문자열의 개수: ${strCount}`
+}
+
+let result = checkStringOrNumber("10", "a10", "a", "7", 7, "8", 10, 6, 1.1, "0")
+console.log(result)
